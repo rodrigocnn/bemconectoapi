51b7d1816238365b96df28a923ac6b02edc26c5b
Rodrigo César <rodrigocnn@gmail.com>
2025-09-13 18:42:20 -0300

    feat:(shared-middlewares) add autorize middleware

---
+++ src/shared/middlewares/authorize.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction } from "express";
+
+export function authorize(roles: string[]) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const user = req.user;
+
+    if (!user || !roles.includes(user.role)) {
+      return res.status(403).json({ message: "Acesso negado" });
+    }
+
+    next();
+  };
+}
