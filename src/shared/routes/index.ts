58f86f1d8e567eb4ea5dce2bc32a46971149d8a5
Rodrigo César <rodrigocnn@gmail.com>
2025-09-13 18:43:55 -0300

    feat:(routes) add new routes users and appointments

--- src/shared/routes/index.ts
+++ src/shared/routes/index.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { patientRoutes } from "../../modules/patient/routes/patient.routes";
+import { appointmentRoutes } from "../../modules/appointment/routes/appointment.routes";
 import psychologistsRoutes from "../../modules/psychologist/routes/psychologist.routes";
 import authRoutes from "../../modules/auth/routes/auth.routes";
+import userRoutes from "../../modules/user/routes/user.routes";
 
 export const router = Router();
 
 router.use(authRoutes);
+router.use(userRoutes);
 router.use(psychologistsRoutes);
 router.use(patientRoutes);
+router.use(appointmentRoutes);
