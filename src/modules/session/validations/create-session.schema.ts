293ec31ab60a0e69fb49120ac43ab77ffaf7b3f4
Rodrigo César <rodrigocnn@gmail.com>
2025-09-17 13:53:25 -0300

    feat:(modules-session) updated props create-session-schema

--- src/modules/session/validations/create-session.schema.ts
+++ src/modules/session/validations/create-session.schema.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 // schema que converte string ISO para Date
 export const createReservationSchema = z.object({
-  dateTime: z.preprocess(
+  sessionDate: z.preprocess(
     (val) => {
       if (typeof val === "string" || val instanceof Date) {
         const date = new Date(val);
@@ -12,6 +12,6 @@ export const createReservationSchema = z.object({
     },
     z.date({ error: "Data e hora inválidas" })
   ),
-  status: z.boolean({ error: "Status deve ser verdadeiro ou falso" }),
-  clientId: z.uuid({ error: "ID do cliente inválido" }),
+  status: z.string({ error: "Status deve ser verdadeiro ou falso" }),
+  patientId: z.uuid({ error: "ID do cliente inválido" }),
 });
