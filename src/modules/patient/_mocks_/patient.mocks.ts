d6bc4f3ab42e1388c7df5afa4a4338ea6e93a3d5
Rodrigo César <rodrigocnn@gmail.com>
2025-10-27 15:31:12 -0300

    test:(mocks) add mocks to patient and user

---
+++ src/modules/patient/_mocks_/patient.mocks.ts
@@ -0,0 +1,11 @@
+import { CreatePatientDTO } from "../dtos/create-patient.dto";
+
+export const patientData: CreatePatientDTO = {
+  psychologistId: "1",
+  name: "Rodrigo César",
+  email: "rodrigo@exemplo.com",
+  birthDate: "1985-05-01",
+  phone: "(11) 99999-0000",
+  cpf: "539.461.300-11",
+  rg: "0928901564",
+};
