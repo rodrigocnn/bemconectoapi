9c7c343af4be937c37f894b0b6869d61bdaeeb52
Rodrigo César <rodrigocnn@gmail.com>
2025-10-27 09:29:09 -0300

    feat(app) add corsOptions

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
-
+import cors from "cors";
 import dotenv from "dotenv";
+
 import { router } from "./shared/routes";
 import { errorHandler } from "./shared/middlewares/errorHandler";
 
@@ -8,9 +9,14 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3333;
 const app = express();
-app.use(express.json());
 
+const corsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
 app.use(router);
 app.use(errorHandler);
-
 app.listen(PORT, () => console.log(`Server is running on port ${PORT} `));
